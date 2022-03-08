b7540d6b8359b34c3d7f342065031e3c3372cf9f
DavidKahumbi <david.kahumbi@student.moringaschool,com>
2022-03-08 23:54:36 +0300

    add index.html index.js styles.css and Readme files

--- index.js
+++ index.js
@@ -1,30 +1,44 @@
-function myButton(); {
+function room() {
     var dob = document.getElementById("dob");
-    var male = docunent.getElementById("male");
+    var male = document.getElementById("male");
     var female = document.getElementById("female");
-    dobArray = dob.ariaValueMax.split("-")
-}
-var akanMale = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var akanFemale = ["Akosua", "Adwoa", "Abenaa", 'Akua', "Yaa", "Afua", "Ama"];
+    dobArray = dob.Value.split("-")
 
-var CC = dobArray[0][0] + dobArray[0][1]
-var YY = dobArray[0][2] + dobArray[0][3]
-var MM = dobArray[1]
-var DD = dobArray[2]
+     console.log(dobArray)
+    var CC = dobArray[0][0] + dobArray[0][1]
+    var YY = dobArray[0][2] + dobArray[0][3]
+    var MM = dobArray[1]
+    var DD = dobArray[2]
 
-var cc = parseInt(CC);
-var yy = parseInt(YY);
-var mm = parseInt(MM);
-var dd = parseInt(DD);
+    var cc = parseInt(CC);
+    var yy = parseInt(YY);
+    var mm = parseInt(MM);
+    var dd = parseInt(DD);
 
-var d = (((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7
+   
 
-if (akanMale[d] === undefined || akanFemale[d] === undefined) {
-    alert("you entered the wrong date")
-} else {
+    var d = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
+     d=parseInt(d)
 
-    if (akanMale === true) {
+    console.log(d)
+
+    var akanMale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var akanFemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+
+    if(akanMale[d] === undefined|| akanFemale[d] === undefined){
+        alert("you entered the wrong date")
+    }else{
+
+        if(male.checked === true){
+        console.log(akanMale[d])
         alert("Your Akan name is" + akanMale[d])
-    }
-    if (akanFemale === true) {
-        alert("your Akan name is" + akanFemale[d]
+    }        
+     if(female.checked === true){
+         console.log(akanFemale[d])
+        
+        alert("your Akan name is" + akanFemale[d])
+     }
+    } 
+
+}
+                        
